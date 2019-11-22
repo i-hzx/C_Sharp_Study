78518834816eb11667f4c8bf00fd582a8f2a478b
i-hzx <39261038+i-hzx@users.noreply.github.com>
2019-11-22 17:04:28 +0800

    建立Guest对象

---
+++ WindowsFormsApp1/Model/Guest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Model
+{
+    class Guest
+    {
+        private int id;
+        private String name;
+        private String email;
+        private String content;
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string Email { get => email; set => email = value; }
+        public string Content { get => content; set => content = value; }
+    }
+}
